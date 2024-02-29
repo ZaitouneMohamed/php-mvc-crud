3aed87f5d400366916977b895b0d9e80fcef352d
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-02-29 22:23:26 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'database' => [
+        "DATABASE_HOST" => 'localhost',
+        "DATABASE_USER" => 'root',
+        "DATABASE_PASS" => '',
+        "DATABASE_NAME" => 'php_poo_mvc_crud',
+    ]
+];
